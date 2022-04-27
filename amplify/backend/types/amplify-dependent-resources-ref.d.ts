203eed8e5e2f611db3f41626c24e8bcc8b81f0cd
ubilanov <umar.uranuulu@gmail.com>
2022-04-27 18:31:00 +0600

    add graph ql

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "amplifyapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "s398560245": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
